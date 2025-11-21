7c602f856e4d8fe8b3e00990ddc2acebf5bcc469
Ben Fairclough <benfairclough@hotmail.co.uk>
2025-11-21 16:55:43 +0000

    Updated scope - remove ipv6

--- files/boot.sh
+++ files/boot.sh
@@ -1,4 +1,5 @@
 /usr/bin/sh
+sed -i 's/.*listen.*:80.*/#listen/g' /etc/nginx/sites-available/default
 RANDOM="$(openssl rand -base64 6)"
 sed -i -e "s/Welcome/$RANDOM-Welcome/g" /var/www/html/index.html 
 /usr/sbin/nginx -g "daemon off;"
